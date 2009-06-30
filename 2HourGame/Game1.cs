86174b54a609f6745c94e81081c655ecc8998f20
Ryan Janzen <lagattack@gmail.com>
2009-06-30 14:29:28 -0600

    Tidied up Game1 class.

--- 2HourGame/Game1.cs
+++ 2HourGame/Game1.cs
@@ -17,7 +17,6 @@ namespace _2HourGame {
   /// </summary>
   public class Game1 : Microsoft.Xna.Framework.Game {
     GraphicsDeviceManager graphics;
-    SpriteBatch spriteBatch;
 
     public Game1() {
       graphics = new GraphicsDeviceManager(this);
@@ -34,52 +33,6 @@ namespace _2HourGame {
       // TODO: Add your initialization logic here
 
       base.Initialize();
-    }
-
-    /// <summary>
-    /// LoadContent will be called once per game and is the place to load
-    /// all of your content.
-    /// </summary>
-    protected override void LoadContent() {
-      // Create a new SpriteBatch, which can be used to draw textures.
-      spriteBatch = new SpriteBatch(GraphicsDevice);
-
-      // TODO: use this.Content to load your game content here
-    }
-
-    /// <summary>
-    /// UnloadContent will be called once per game and is the place to unload
-    /// all content.
-    /// </summary>
-    protected override void UnloadContent() {
-      // TODO: Unload any non ContentManager content here
-    }
-
-    /// <summary>
-    /// Allows the game to run logic such as updating the world,
-    /// checking for collisions, gathering input, and playing audio.
-    /// </summary>
-    /// <param name="gameTime">Provides a snapshot of timing values.</param>
-    protected override void Update(GameTime gameTime) {
-      // Allows the game to exit
-      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-        this.Exit();
-
-      // TODO: Add your update logic here
-
-      base.Update(gameTime);
-    }
-
-    /// <summary>
-    /// This is called when the game should draw itself.
-    /// </summary>
-    /// <param name="gameTime">Provides a snapshot of timing values.</param>
-    protected override void Draw(GameTime gameTime) {
-      GraphicsDevice.Clear(Color.CornflowerBlue);
-
-      // TODO: Add your drawing code here
-
-      base.Draw(gameTime);
     }
   }
 }
