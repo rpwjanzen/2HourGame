2bdd0b5a48010987d84fff79cbda3689cbdf64d1
Ian Desktop <ianhern@gmail.com>
2009-07-27 16:38:24 -0600

    Updated the smoke and splash animations.

--- 2HourGame/EffectManager.cs
+++ 2HourGame/EffectManager.cs
@@ -23,7 +23,7 @@ namespace _2HourGame
             textureInfos = new Dictionary<String, AnimatedTextureInfo>();
 
             textureInfos.Add("goldPickup", new AnimatedTextureInfo(new Vector2(30, 100), 9, 9, 0.3f, true, new Vector2(0, -50)));
-            textureInfos.Add("splash", new AnimatedTextureInfo(new Vector2(30, 70), 10, 10, 1f, true, new Vector2(0, -30)));
+            textureInfos.Add("splash", new AnimatedTextureInfo(new Vector2(30, 30), 10, 10, 1f, true, new Vector2(0, -15)));
             textureInfos.Add("cannonSmoke", new AnimatedTextureInfo(new Vector2(50, 50), 6, 2, 1f, true, new Vector2(0, 0)));
         }
 
