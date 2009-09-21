938543d3c16ff9a8c72ffa2737fe2ad0e63dbc35
Ryan Janzen <rpwjanzen@gmail.com>
2009-09-21 13:40:48 -0600

    Git now ignores SourceMonitor files. We can now display arbitrary progress bars.

---
+++ 2HourGame/View/ProgressBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using _2HourGame.View.GameServices;
+
+namespace _2HourGame.View
+{
+    class ProgressBar
+    {
+        Texture2D Texture { get; set; }
+        Vector2 Origin { get; set; }
+        float ZIndex { get; set; }
+
+        public Color FillColor { get; set; }
+        public Color EmptyColor { get; set; }
+        public float Scale { get; set; }
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// [0.0f - 1.0f]
+        /// </summary>
+        public float Progress { get; set; }
+
+        public ProgressBar(ITextureManager textureManager) {
+            FillColor = Color.Green;
+            EmptyColor = Color.Yellow;
+            Scale = 1.0f;
+
+            Origin = textureManager.getTextureOrigin("progressBar", Scale); 
+            Texture = textureManager.getTexture("progressBar");
+            ZIndex = ZIndexManager.getZIndex(ZIndexManager.drawnItemOrders.healthBar);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            var progressSoFarRectangle = new Rectangle(0, 0, (int)(Texture.Width * Progress), Texture.Height);
+            spriteBatch.Draw(Texture, Position, null, EmptyColor, 0f, Origin, Scale, SpriteEffects.None, ZIndex + 0.001f);
+            spriteBatch.Draw(Texture, Position, progressSoFarRectangle, FillColor, 0f, Origin, Scale, SpriteEffects.None, ZIndex);
+        }
+    }
+}
