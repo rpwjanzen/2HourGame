2b1453d41be5a84c80fba6cf141c87c6ebcc3241
Ryan Janzen <rpwjanzen@gmail.com>
2009-09-23 20:46:26 -0600

    Controller keys can now be mapped to actions.

--- 2HourGame/Controller/ShipControllBehaviours.cs
+++ 2HourGame/Controller/ShipControllBehaviours.cs
@@ -12,29 +12,46 @@ namespace _2HourGame.Controller
 {
     static class ShipControllBehaviours
     {
-        public static void FireCannons(GamePadState gs, GamePadState previousGamePadState, Player player, GameTime gameTime) 
+        public enum Action { FireLeftCannon, FireRightCannon, PickupGold, RepairShip };
+
+        static Dictionary<Action, Buttons> actionKeys;
+
+        static ShipControllBehaviours()
         {
-            if (gs.IsButtonDown(Buttons.LeftTrigger))
+            actionKeys = new Dictionary<Action, Buttons>();
+
+            actionKeys.Add(Action.FireLeftCannon, Buttons.LeftTrigger);
+            actionKeys.Add(Action.FireRightCannon, Buttons.RightTrigger);
+            actionKeys.Add(Action.PickupGold, Buttons.A);
+            actionKeys.Add(Action.RepairShip, Buttons.B);
+        }
+
+
+        public static void FireCannons(GamePadState gs, GamePadState previousGamePadState, Player player, GameTime gameTime)
+        {
+            if (gs.IsButtonDown(actionKeys[Action.FireLeftCannon]))
             {
                 player.FireCannon(gameTime, CannonType.LeftCannon);
             }
-            if (gs.IsButtonDown(Buttons.RightTrigger))
+            if (gs.IsButtonDown(actionKeys[Action.FireRightCannon]))
             {
                 player.FireCannon(gameTime, CannonType.RightCannon);
             }
         }
 
-        public static void PickupGold(GamePadState gs, GamePadState previousGamePadState, Player player, GameTime gameTime) 
+        public static void PickupGold(GamePadState gs, GamePadState previousGamePadState, Player player, GameTime gameTime)
         {
-            if (gs.IsButtonDown(Buttons.A) && previousGamePadState.IsButtonUp(Buttons.A)) 
+            var pickupGoldButton = actionKeys[Action.PickupGold];
+            if (gs.IsButtonDown(pickupGoldButton) && previousGamePadState.IsButtonUp(pickupGoldButton))
             {
                 player.AttemptPickupGold(gameTime);
             }
         }
 
-        public static void RepairShip(GamePadState gs, GamePadState previousGamePadState, Player player, GameTime gameTime) 
+        public static void RepairShip(GamePadState gs, GamePadState previousGamePadState, Player player, GameTime gameTime)
         {
-            if (gs.IsButtonDown(Buttons.B) && previousGamePadState.IsButtonUp(Buttons.B))
+            var repairButton = actionKeys[Action.RepairShip];
+            if (gs.IsButtonDown(repairButton) && previousGamePadState.IsButtonUp(repairButton))
                 player.AttemptRepair();
         }
     }
