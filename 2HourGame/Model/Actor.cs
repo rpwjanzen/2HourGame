5e50adfba2ca9851357a28e75690b2588ee82d64
Ryan Janzen <rpwjanzen@gmail.com>
2009-10-27 01:14:42 -0600

    Beginning of the horror of moving away from GameComponents!

---
+++ 2HourGame/Model/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace _2HourGame.Model {
+    class Actor {
+        public virtual void Draw(GameTime gameTime) { }
+        public virtual void Update(GameTime gameTime) { }
+        public virtual void LoadContent(ContentManager content) { }
+    }
+}
---
+++ 2HourGame/Model/IActor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace _2HourGame.Model {
+    interface IActor {
+        void Draw(GameTime gameTime);
+        void Update(GameTime gameTime);
+        void LoadContent(ContentManager content);
+    }
+}
